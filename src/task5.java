4523f78176c88970ae864dedb416a46ae010233b
Maksym Kletsman <mkletsm1@stu.vistula.edu.pl>
2025-11-12 04:05:09 +0100

    task 5 commit

--- src/task5.java
+++ src/task5.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
 
-public class task5 {
+import java.util.Scanner;
+
+public class task5
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter word: ");
+        String word = sc.nextLine();
+
+        word = word.toLowerCase();
+
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        if (isPalindrome) {
+            System.out.println("word is palidrome");
+        }
+        else
+        {
+            System.out.println("This word is not palindrome");
+        }
+
+        sc.close();
+
+    }
 }
