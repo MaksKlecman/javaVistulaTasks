0268af8e868da8c1657373d18ed9fdc1037df95c
Maksym Kletsman <mkletsm1@stu.vistula.edu.pl>
2025-11-12 04:05:09 +0100

    task 3 remake

--- src/task3.java
+++ src/task3.java
@@ -1,44 +1,25 @@
-   public class task3
-
-   {
-       public static void main(String[] args)
-       {
-            int[][] matrix = new int[10][10];
-             int sum = 0;
+ class task3 {
+     public static void main(String[] args)
+     {
+         int[][] matrix = new int[10][10];
+         int sum = 0;
 
 
-           for(int i = 0; i < 10; i++ )
-           {
-               for (int j = 0; j < 10; j++)
+         for (int i = 0; i <= 9; i++)
+         {
+             for (int j = 0; j <= 9; j++)
+             {
+               if (i == j)
                {
-                   if (i == j)
-                   {
-                       matrix[i][j] = i;
-                       sum = sum + matrix[i][j];
-                   }
-                   else
+                   matrix[i][j] = i;
+                   sum += i;
 
-                   {
-                       matrix[i][j] = 0;
-                   }
-               }
-           }
-
-           System.out.println("matrix:");
-           for (int i = 0; i < 10; i++)
-
-           {
-               for (int j = 0; j < 10; j++)
-               {
-                   System.out.print(matrix[i][j] + " ");
                }
 
-                 System.out.println();
-
-
-           }
-
-           System.out.println("sum of dagonal =  "  + sum);
-
-   }
-}
+                 System.out.print(matrix[i][j] + " ");
+             }
+             System.out.println();
+         }
+         System.out.println("Sum of diagonal elements: " + sum);
+     }
+ }
