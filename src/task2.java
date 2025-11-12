ca0c35e1f1ae6217c769ec0b9b7a9e897c347503
Maksym Kletsman <mkletsm1@stu.vistula.edu.pl>
2025-11-12 03:36:28 +0100

    task2 commit

--- src/task2.java
+++ src/task2.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
 
-public class task2 {
+import java.util.Random;
+
+
+public class task2
+ {
+    public static void main(String[] args)
+
+    {
+        int[] arr = new int[10];
+          Random rnd = new Random();
+
+          System.out.println("Array: ");
+
+          for (int i = 0; i < arr.length; i++)
+            {
+             arr[i] = rnd.nextInt(100) + 1;
+
+             System.out.println(arr[i] + "    ");
+
+          }
+
+          System.out.println();
+
+            int min = arr[0];
+             int max = arr[0];
+
+             for (int v : arr)
+             {
+                  if (v < min) min =  v;
+                  if (v > max)  max = v;
+             }
+
+          System.out.println("min = " + min);
+        System.out.println("Max = " + max);
+
+
+    }
+
 }
